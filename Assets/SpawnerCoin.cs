9ba9bca3ecb8140d4fcc11aff04d782f135c3b4a
Tan Kenji <kenjitan1020@gmail.com>
2023-09-25 16:23:40 +0800

    Initial Commitment

---
+++ Assets/SpawnerCoin.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerCoin : MonoBehaviour
+{
+
+    public GameObject PrefabCoin;
+
+    public float DurationLeft = 60f;
+
+    public float SpawnInterval = 1f;
+
+    public float MinX = -1f;
+    public float MaxX = 1f;
+
+    public float CoinsCollected = 0f;
+    public TMP_Text Text;
+
+    private float randomX;
+
+    private float spawnTimer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnTimer = SpawnInterval;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (spawnTimer > 0)
+        {
+            spawnTimer -= Time.deltaTime;
+        }
+
+        else
+        {
+            float rand = Random.Range(MinX, MaxX);
+
+            Vector3 randomPos = new Vector3(rand, 0f, 0f);
+
+            if(PrefabCoin != null)
+            {
+                GameObject.Instantiate(PrefabCoin, position:transform.position + randomPos, transform.rotation);
+            }
+            Debug.Log(message:"Times up, should spawn coin");
+            spawnTimer = SpawnInterval;
+        }
+    }
+
+    public void AddCoins(float amount)
+    {
+        CoinsCollected += amount;
+        Debug.Log(message:"Collected coins " + CoinsCollected);
+
+        if(Text != null)
+        {
+            Text.text = CoinsCollected.ToString();
+        }
+    }
+}
---
+++ Assets/coin.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class coin : MonoBehaviour
+{
+    public int CoinsAmount = 10;
+    public AudioSource _AudioSource;
+    public GameObject CoinCollectedAudio;
+
+    public Collider2D _Collider2D;
+
+    private SpawnerCoin _spawner;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        _Collider2D = GetComponent<Collider2D>();
+
+        _spawner = FindObjectOfType<SpawnerCoin>();
+
+        _AudioSource = GetComponent<AudioSource>();
+    }
+
+    private void OnMouseOver()
+    {
+
+        if (CoinCollectedAudio != null)
+        {
+           GameObject.Instantiate(CoinCollectedAudio, transform.position, Quaternion.identity);
+        }
+
+        if (Effects != null)
+        {
+            GameObject.Instantiate(Effects, transform.position, Quaternion.identity);
+        }
+
+
+        if (_spawner != null)
+        {
+            // spawner will add points to CoinsCollected
+            _spawner.AddCoins(CoinsAmount);
+        }
+
+        Destroy(this.gameObject);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
